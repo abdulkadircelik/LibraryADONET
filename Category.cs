37ce364f2ac73101e2d5fee538977a614b62a37e
Zafer CÖMERT <zafer.comert@hotmail.com>
2020-11-14 20:28:13 +0300

    Week 06

---
+++ Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryADONET
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public void Add(Category category)
+        {
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO Categories VALUES (@CategoryName)"))
+            {
+                cmd.Parameters.AddWithValue("CategoryName", category.CategoryName);
+                VTYS.SqlExecuteNonQuery(cmd);
+            }
+        }
+    }
+}
---
+++ VTYS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LibraryADONET
+{
+    public class VTYS
+    {
+        private static string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LibraryDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static bool SqlExecuteNonQuery(SqlCommand cmd)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    conn.Close();
+                    conn.Dispose();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static SqlDataReader SqlExecuteReader(SqlCommand cmd)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            cmd.Connection = conn;
+            return cmd.ExecuteReader();
+        }
+    }
+}
