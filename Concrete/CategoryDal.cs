e9bf4e9c41a44affc36f692e7af50005e70b010b
Zafer CÖMERT <zafer.comert@hotmail.com>
2020-11-19 15:54:35 +0300

    CategoryDal has been updated.

--- Concrete/CategoryDal.cs
+++ Concrete/CategoryDal.cs
@@ -12,27 +12,66 @@ namespace LibraryADONET.Concrete
     {
         public void Add(Category entity)
         {
-            throw new NotImplementedException();
+            using (SqlCommand cmd  = new SqlCommand("INSERT INTO Categories(CategoryName) VALUES (@CategoryName)"))
+            {
+                cmd.Parameters.AddWithValue("CategoryName", entity.CategoryName);
+                VTYS.SqlExecuteNonQuery(cmd);
+            }
         }
 
         public void Delete(Category entity)
         {
-            throw new NotImplementedException();
+            using (SqlCommand cmd = new SqlCommand("DELETE FROM Categories WHERE CategoryId = @CategoryId"))
+            {
+                cmd.Parameters.AddWithValue("CategoryId", entity.CategoryId);
+                VTYS.SqlExecuteNonQuery(cmd);
+            }
         }
 
         public List<Category> GetAll()
         {
-            throw new NotImplementedException();
+            var categoryList = new List<Category>();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Categories");
+            var reader = VTYS.SqlExecuteReader(cmd);
+
+            while (reader.Read())
+            {
+                Category category = new Category
+                {
+                    CategoryId = Convert.ToInt32(reader[0]),
+                    CategoryName = reader[1].ToString()
+                };
+
+                categoryList.Add(category);
+            }
+            return categoryList;
         }
 
         public Category GetById(int id)
         {
-            throw new NotImplementedException();
+            Category category = new Category();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Categories WHERE CategoryId= @CategoryId");
+            cmd.Parameters.AddWithValue("CategoryId", id);
+            
+            var reader = VTYS.SqlExecuteReader(cmd);
+
+            while (reader.Read())
+            {
+                category.CategoryId = Convert.ToInt32(reader[0]);
+                category.CategoryName = reader[1].ToString();
+            }
+            return category;
         }
 
         public void Update(Category entity)
         {
-            throw new NotImplementedException();
+            using (SqlCommand cmd = 
+                new SqlCommand("UPDATE Categories SET CategoryName = @CategoryName WHERE CategoryId = @CategoryId"))
+            {
+                cmd.Parameters.AddWithValue("CategoryName", entity.CategoryName);
+                cmd.Parameters.AddWithValue("CategoryId", entity.CategoryId);
+                VTYS.SqlExecuteNonQuery(cmd);
+            }
         }
     }
 }
